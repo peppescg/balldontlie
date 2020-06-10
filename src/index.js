ba28a32d2ed126397b5c7847f1de00b398d15942
peppescg <peppescg@gmail.com>
2020-06-11 00:02:24 +0200

    add routes, theme, recoilState

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { createHistory, LocationProvider } from "@reach/router";
+import { RecoilRoot } from "recoil";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { 500: "#467fcf" },
+  },
+});
+
+let history = createHistory(window);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <LocationProvider history={history}>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
+      </ThemeProvider>
+    </React.StrictMode>
+  </LocationProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/routes/Detail.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Detail = () => (
+  <div>
+    <h2>Dashboard</h2>
+  </div>
+);
+
+export default Detail;
